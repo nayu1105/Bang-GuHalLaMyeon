3b2449923e43859c254229208c7297cf3b9c74af
dbwls387 <dbwls387@naver.com>
2022-11-13 21:48:07 +0900

    design

--- Front/src/components/HomeTwo/BannerArea.vue
+++ Front/src/components/HomeTwo/BannerArea.vue
@@ -2,6 +2,20 @@
   <section class="banner__area pb-110">
     <div class="container">
       <div class="row">
+        <div class="col-xxl-4 offset-xxl-4">
+          <div class="section__title-wrapper mb-60 text-center">
+            <h2 class="section__title">
+              Current
+              <span class="yellow-bg yellow-bg-big"
+                >Events<img src="../../assets/img/shape/yellow-bg.png" alt=""
+              /></span>
+            </h2>
+            <p>이벤트 참여하고 혜택 받아가세요!</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
         <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
           <div
             class="banner__item p-relative mb-40"
@@ -12,17 +26,14 @@
             }"
           >
             <div class="banner__content">
-              <span>Free</span>
+              <span>Event</span>
               <h3 class="banner__title">
-                <router-link to="/course-details"
-                  >Germany Foundation <br />
-                  Document</router-link
-                >
+                <router-link to="/course-details">Open Event <br />월세 지원 이벤트!</router-link>
               </h3>
-              <router-link to="/courses" class="e-btn e-btn-2">View Courses</router-link>
+              <router-link to="/courses" class="e-btn e-btn-2">이벤트 참여하기</router-link>
             </div>
             <div class="banner__thumb d-none d-sm-block d-md-none d-lg-block">
-              <img src="../../assets/img/banner/banner-img-1.png" alt="" />
+              <!-- <img src="../../assets/img/banner/banner-img-1.png" alt="" /> -->
             </div>
           </div>
         </div>
@@ -36,16 +47,16 @@
             }"
           >
             <div class="banner__content">
-              <span class="orange">new</span>
+              <span class="orange">Event</span>
               <h3 class="banner__title">
                 <router-link to="/course-details"
-                  >Online Courses <br />From Eduka University
+                  >회원가입 이벤트 <br />지금 가입하면 포인트 증정!
                 </router-link>
               </h3>
-              <router-link to="/courses" class="e-btn e-btn-2">Find Out More</router-link>
+              <router-link to="/courses" class="e-btn e-btn-2">회원가입</router-link>
             </div>
             <div class="banner__thumb banner__thumb-2 d-none d-sm-block d-md-none d-lg-block">
-              <img src="../../assets/img/banner/banner-img-2.png" alt="" />
+              <!-- <img src="../../assets/img/cta/cta-shape-2.png" alt="" /> -->
             </div>
           </div>
         </div>
@@ -56,6 +67,12 @@
 
 <script>
 export default {
-  name: 'BannerArea',
+  name: "BannerArea",
 };
 </script>
+
+<style scoped>
+.banner__area {
+  margin-top: 80px;
+}
+</style>
--- Front/src/components/HomeTwo/HeroArea.vue
+++ Front/src/components/HomeTwo/HeroArea.vue
@@ -1,101 +1,120 @@
 <template>
-   <section id="sectionImg"
-     class="hero__area hero__height hero__height-2 d-flex align-items-center blue-bg-3 p-relative fix"
-   >
-     <div class="container container-80">
-       <div class="hero__content-wrapper mt-90">
-         <div class="main-box align-items-center">
-           <div class="col-xxl">
-             <div class="hero__content hero__content-2 p-relative z-index-1">
-               <h3 class="hero__title hero__title-2">
-                 Find Your&nbsp;<span class="perfect">Perfect</span>&nbsp;Home <br />
-                 
-               </h3>
- 
-               <div class="hero__search">
-                 <!--  -->
-                 <div id="selectForm">
-                   <select
-                     id="selectDo"
-                     class="form-select mt-5"
-                     aria-label="Default select example"
-                   >
-                     <option selected>도/광역시</option>
-                     <option value="1">One</option>
-                     <option value="2">Two</option>
-                   </select>
-                   <select class="form-select mt-5" aria-label="Default select example">
-                     <option selected>시/군/구</option>
-                     <option value="1">One</option>
-                     <option value="2">Two</option>
-                   </select>
-                   <select class="form-select mt-5 mb-5" aria-label="Default select example">
-                     <option selected>동</option>
-                     <option value="1">One</option>
-                     <option value="2">Two</option>
-                   </select>
- 
-                   <button type="button" class="btn btn-light mb-5">Search</button>
-                 </div>
-               </div>
-             </div>
-           </div>
-         </div>
-       </div>
-     </div>
-   </section>
- </template>
- 
- <script>
- export default {
-   name: 'HeroArea',
- };
- </script>
- 
- <style scoped>
- .form-select {
-   width: 25%;
-   margin-right: 5px;
-   display: inline-block;
- }
- 
- #selectForm {
-    width: 80%;
-   display: flex;
-   justify-content: center;
- }
- 
- .hero__title{
-   display: flex;
-   justify-content: center;
- }
- 
- .container-80{
-    width: 70%;
-    padding-bottom: 130px;
- }
- 
- .main-box {
-   display: flex;
-   justify-content: center;
- }
- 
- .hero__search{   
-   display: flex;
-   justify-content: center;
- }
+  <section
+    id="sectionImg"
+    class="hero__area hero__height hero__height-2 d-flex align-items-center blue-bg-3 p-relative fix"
+  >
+    <div class="container container-80">
+      <div class="hero__content-wrapper mt-50">
+        <div class="main-box align-items-center">
+          <div class="col-xxl">
+            <div class="hero__content hero__content-2 p-relative z-index-1">
+              <h3 class="hero__title hero__title-2">
+                Find Your&nbsp;<span class="perfect">Perfect</span>&nbsp;Home <br />
+              </h3>
 
- #sectionImg {
+              <div class="hero__search">
+                <!--  -->
+                <div id="selectForm">
+                  <select
+                    id="selectDo"
+                    class="form-select mt-5"
+                    aria-label="Default select example"
+                  >
+                    <option selected>도/광역시</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                  </select>
+
+                  <!-- <button type="button" class="toggle-btn">
+                    도/광역시
+                    <img src="../../assets/img/icon/select-arrow.svg" alt="" class="ico-down" />
+                  </button>
+                  <ul class="selectbox-option hide">
+                    <li><button type="button" class="option-btn">Python</button></li>
+                    <li><button type="button" class="option-btn">Java</button></li>
+                    <li><button type="button" class="option-btn">JavaScript</button></li>
+                    <li><button type="button" class="option-btn">C#</button></li>
+                    <li><button type="button" class="option-btn">C/C++</button></li>
+                  </ul> -->
+
+                  <select class="form-select mt-5" aria-label="Default select example">
+                    <option selected>시/군/구</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                  </select>
+                  <select class="form-select mt-5" aria-label="Default select example">
+                    <option selected>동</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                  </select>
+
+                  <button type="button" class="select-btn btn btn-light mt-5">Search</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "HeroArea",
+};
+</script>
+
+<style scoped>
+.form-select {
+  width: 25%;
+  margin-right: 5px;
+  display: inline-block;
+}
+
+#selectForm {
+  width: 80%;
+  display: flex;
+  justify-content: center;
+}
+
+.form-select,
+.select-btn {
+  border-radius: 12px;
+  margin: 3px;
+  padding: 8px;
+}
+
+.hero__title {
+  display: flex;
+  justify-content: center;
+}
+
+.container-80 {
+  width: 70%;
+  padding-bottom: 130px;
+}
+
+.main-box {
+  display: flex;
+  justify-content: center;
+}
+
+.hero__search {
+  display: flex;
+  justify-content: center;
+}
+
+#sectionImg {
   background-image: url("../../assets/img/hero/home_bg2.jpg");
   background-size: cover;
- }
+}
 
- .hero__title {
+.hero__title {
   color: white;
- }
+}
 
- .perfect {
+.perfect {
   color: rgb(240, 195, 50);
- }
- </style>
- 
+}
+</style>
