654db8ea0a9f228f7574dfe615f7db844f44f944
dbwls387 <dbwls387@naver.com>
2022-11-21 20:59:15 +0900

    review: BE

--- whereismyhome_5_spring_7/src/main/java/com/mycom/myapp/review/service/ReviewService.java
+++ whereismyhome_5_spring_7/src/main/java/com/mycom/myapp/review/service/ReviewService.java
@@ -1,9 +1,13 @@
 package com.mycom.myapp.review.service;
 
+import com.mycom.myapp.review.dto.ReviewDto;
 import com.mycom.myapp.review.dto.ReviewParamDto;
 import com.mycom.myapp.review.dto.ReviewResultDto;
 
 public interface ReviewService {
+	ReviewResultDto reviewInsert(ReviewDto dto);
+	ReviewResultDto reviewDelete(int reviewId);
+	
 	ReviewResultDto reviewList(ReviewParamDto reviewParamDto);
 	int reviewListTotalCnt();
 	
